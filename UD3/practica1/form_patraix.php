9e2122a83a642aac413baab0afc480ec18badce3
Enrique Castellón <enrcasher@alu.edu.gva.es>
2025-10-16 21:26:00 +0200

    Change form method to POST and update options

--- UD3/practica1/form_patraix.php
+++ UD3/practica1/form_patraix.php
@@ -14,32 +14,28 @@
 
         <h2>Consulta datos por Barrios(Distrito de Patraix)</h2>
 
-        <form name = "form1" method = "get" action="distritos_ctl.php">
+        <form name = "form1" method = "post" action="patraix_ctl.php">
             <p>Barrio:
                 <?php
                     include 'datos_patraix.php';
-
-                    echo '<select name="distrito">';
-                    foreach ($datos_distritos as $barrio => $poblacion) {
-                        if ($barrio == 'Patraix'){
-                            echo '<option value="' . $barrio . '" selected>' . $barrio . '</option>';
-                        } else {
-                            echo '<option value="' . $barrio . '">' . $barrio . '</option>';
-                        }
-                    }
+                    echo '<select name="barrrio">';
+                    echo '<option value="0" selected>mostrar todos</option>';
+                    foreach ($datos_patraix as $indice => $itera) {
+                            $barrio=$value[0];
+                            echo '<option value="' . $indice . '" selected>' . $barrio . '</option>';
+                                          }
                     echo '</select>'; 
             ?>
-            </p>
-            <p>
-                <input type="checkbox" name="muestra_todo"> Mostrar todos los Barrios <br>
-            </p>   
+            </p> 
             <p>
                 <input type="submit" name="Submit" value="Consultar">
             </p>
             </form>
-
+        
 
             <?php
+                echo '<input type="hidden" name="nombre" value="'.$nombre.'">';
+                echo '<input type="hidden" name="apellidos" value="'.$apellidos.'">';
                 echo '<br><br><a href="menu.php?nombre=' . $nombre . '&apellidos=' . $apellidos . '">Volver</a>';
             ?>
     </body>
