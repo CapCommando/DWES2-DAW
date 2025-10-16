d48646ae64c2a10c5cf20718c857ca2abadb5ab9
Enrique Castellón <enrcasher@alu.edu.gva.es>
2025-10-16 19:25:41 +0200

    Preparando el ctl

--- UD3/practica1/distritos_ctl.php
+++ UD3/practica1/distritos_ctl.php
@@ -1,3 +1,7 @@
 <?php
-
+$nombre=$_GET['nombre'];
+$apellidos=$_GET['apellidos'];
+include datos_distritos.php;
+echo '<h3>!Bienvenido '. $nombre.' '.$apellidos.'!</h3>';
+echo'<br><br><a href="menu.php?nombre='.$nombre.'&apellidos'.$apellidos">';
 ?>
