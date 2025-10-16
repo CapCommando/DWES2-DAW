48d6536f92a6d540c3ae67c565b26081f6eeff9b
Enrique Castellón <enrcasher@alu.edu.gva.es>
2025-10-16 18:47:57 +0200

    mejorando el  menu

--- UD3/practica1/menu.php
+++ UD3/practica1/menu.php
@@ -2,8 +2,8 @@
 $nombre=$_GET['nombre'];
 $apellido=$_GET['apellido'];
 echo "Bienvenido ". $nombre . $apellido ."!";
-echo '<a href="vacia1.php">Datos por distritos</a>';
-echo '<a href="vacia2.php">Datos por barrios(distrito de Patraix)</a>';
+echo '<a href="form_distritos.php?nombre=".$nombre."&apellidos".$apellidos">Datos por distritos</a>';
+echo '<a href="form_patraix.php?nombre=".$nombre."&apellidos".$apellidos">Datos por barrios(distrito de Patraix)</a>';
 echo '<a href="index.html">Inicio</a>';
 
 
